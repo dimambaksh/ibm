b0f0b011912d6cba567a6f89936fdd0a6425b738
Duane Imambaksh <dimambaksh@gmail.com>
2024-07-28 14:26:00 -0400

    Added redux, scss and base styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import store from "./store.js"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -5,6 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
