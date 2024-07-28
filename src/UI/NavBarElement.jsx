f742dd6a1b83bd66d078445b019a016bb4fdcaac
Duane Imambaksh <dimambaksh@gmail.com>
2024-07-28 18:03:42 -0400

    NavBarElement

---
+++ src/UI/NavBarElement.jsx
@@ -0,0 +1,13 @@
+import "./ui.scss";
+
+function NavBarElement(props){
+    return(
+        <section>
+            <article>
+                <h1>Nav Bar</h1>
+            </article>
+        </section>
+    )
+}
+
+export default NavBarElement;
