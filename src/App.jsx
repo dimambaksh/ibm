07e671f435af25227f3d9947e7af2060f3a552f1
Duane Imambaksh <dimambaksh@gmail.com>
2024-07-28 20:11:57 -0400

    BodyElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import NavBarElement from './UI/NavBarElement';
+import BodyElement from './UI/BodyElement';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
       <NavBarElement/>
+      <BodyElement/>
+    </>
   )
 }
 
---
+++ src/UI/BodyElement.jsx
@@ -0,0 +1,13 @@
+import "./ui.scss";
+
+function BodyElement(props){
+    return(
+        <section className="pagebody">
+            <article className="pagebody__article">
+                <h1>Body</h1>
+            </article>
+        </section>
+    )
+}
+
+export default BodyElement;
